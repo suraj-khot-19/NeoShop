36c190183f2867bb28e5e74886f328e7c244cfaf
suraj-khot-19 <surajkhot19082001@gmail.com>
2025-02-26 10:58:31 +0530

    add repository package with product repo

---
+++ src/main/java/com/suraj/NeoShop/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.suraj.NeoShop.repository;
+
+import com.suraj.NeoShop.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product,Long> {
+}
