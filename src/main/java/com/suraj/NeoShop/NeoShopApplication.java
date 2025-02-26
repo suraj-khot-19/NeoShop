03eb429f65c603433bcca8ea5c766afe3e140e75
suraj-khot-19 <surajkhot19082001@gmail.com>
2025-02-26 09:36:20 +0530

    initial commit

---
+++ src/main/java/com/suraj/NeoShop/NeoShopApplication.java
@@ -0,0 +1,13 @@
+package com.suraj.NeoShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeoShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeoShopApplication.class, args);
+	}
+
+}
